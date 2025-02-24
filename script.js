fe4f22eaf1776cc08e9bf89990ad5bdee27887bd
MegaEmme <mikmusco@gmail.com>
2025-02-24 16:13:31 +0100

    feat: created numeric variables

---
+++ script.js
@@ -0,0 +1,11 @@
+// Chiedo all'utente il *numero di km* e lo salvo in una variabile
+const kmDistance = prompt ('Inserisci la distanza in km');
+console.log(parseInt(kmDistance))
+
+// Chiedo all'utente l'*età* e la salvo in una variabile
+const age = prompt('Inserisci la tua età');
+console.log(parseInt(age))
+
+// Calcolo il *prezzo del biglietto* e lo slavo il una variabile
+const totalPrice = (kmDistance * 0.21)
+console.log(totalPrice)
